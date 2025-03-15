a1e9b9a6cd95ffd18d79d85e25cc97951a34dc17
Morgan Christiansson <git@mog.se>
2025-03-15 01:44:26 +0100

    Add spectator island noise-expression

--- map-generation/bb.lua
+++ map-generation/bb.lua
@@ -3,7 +3,11 @@ data:extend({
     type = "noise-expression",
     name = "elevation",
     intended_property = "elevation",
-    expression = "if((abs(y+1) < 22) - (abs(x) < 10), -inf, elevation_nauvis)",
+    expression = "elevation_nauvis + river + island",
+    local_expressions = {
+      river = "if(abs(y+1) < 22, -inf, 0)",
+      island = "if(distance <= 9, inf, 0)",
+    },
     localised_name = {"noise-expression.elevation_nauvis"}
   }
 })
